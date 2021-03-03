583a56ada632ba0e2ee41fd879dceec2daca748d
baylagas <balbino_a@hotmail.com>
2021-03-02 19:06:57 -0600

    added events and concepts of functions

---
+++ js-events/script.js
@@ -0,0 +1,10 @@
+function showAlert(){
+    alert("this is the alert message")
+    document.getElementById("message1").innerText = "this will be the new text"
+}
+
+function prepareMyBtn(){
+    document.getElementById("myBtn").addEventListener("click", function(){
+        
+    })
+}
--- js-functions/script.js
+++ js-functions/script.js
@@ -1,6 +1,20 @@
-function add(number1, number2){
-    var result = number1 + number2;
-    document.getElementById("message").innerText = "result: " + result
+function add(number1, number2, elementId){
+    var result = number1 + number2
+    var paragraph
+    switch (elementId) {
+        case 1:
+            paragraph = "message1"
+            break;
+        case 2:
+            paragraph = "message2"
+            break;
+        case 3:
+            paragraph = "message3"
+            break;
+        default:
+            break;
+    }
+    document.getElementById(paragraph).innerText = ("result: " + (number1 + number2))
 }
 
-add(3,7)
+add(3,7,2) //10
