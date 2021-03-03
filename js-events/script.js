2ed15d5141532b9e6787a41a06884b6d812dbc3e
baylagas <balbino_a@hotmail.com>
2021-03-02 20:25:39 -0600

    more on events

--- js-events/script.js
+++ js-events/script.js
@@ -3,8 +3,48 @@ function showAlert(){
     document.getElementById("message1").innerText = "this will be the new text"
 }
 
-function prepareMyBtn(){
-    document.getElementById("myBtn").addEventListener("click", function(){
-        
-    })
+function showOut(){
+    alert("i went out of the button")
 }
+
+function prepareMyBtn(){
+    var random = 0
+    var num1 = 4
+    var num2 = 7
+
+    /*
+    document.getElementById("myBtn").addEventListener("click", function(){
+        random = Math.floor(Math.random() * 10)
+        displayOnConsole( random )
+        sendToMessageBox( random )
+    })
+    */
+    document.getElementById("myBtn").addEventListener("click", createRandomNumber)
+
+
+    document.getElementById("myBtn").addEventListener("mouseout", function(){
+        var doubleRandom = random*2
+        sendToMessageBox( doubleRandom )
+    })
+
+    //document.getElementById("myBtn").removeEventListener("click", createRandomNumber);
+}
+
+
+function createRandomNumber(){
+    random = Math.floor(Math.random() * 20)
+    displayOnConsole( random )
+    sendToMessageBox( random )
+}
+
+
+function sendToMessageBox(randomNumber){
+    document.getElementById("message2").innerText = randomNumber
+}
+
+
+function displayOnConsole(number){
+    console.log(number)
+}
+
+prepareMyBtn()
